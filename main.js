69733be0af7809b6413acb21065c940463caa0bf
Burevestnick <bychkov1058@inbox.ru>
2024-06-22 18:14:35 +0300

    clean

--- main.js
+++ main.js
@@ -1,16 +1,4 @@
-title = "    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo, alias. Veritatis perferendis provident maiores voluptas!    "
 
-
-const delSpace = function(str) {
-  str = str.replace(/^\s+/, '')
-  str = str.replace(/\s+$/, '')
-  if (str.length > 30) {
-    str = str.slice(0, 30) + "..."
-  }
-  return str
-} 
-
-console.log(delSpace(title));
 
 
 
